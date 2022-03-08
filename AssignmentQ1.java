e4ea332c184a654a0d64e333d1098886a9069020
basilk1234 <92813253+basilk1234@users.noreply.github.com>
2022-03-07 19:33:34 -0500

    Add files via upload

---
+++ AssignmentQ1.java
@@ -0,0 +1,43 @@
+
+public class AssignmentQ1 {
+
+	public static void main(String[] args) {
+
+		//-----------------------------------
+		// Assignment 1
+		//written by Basil Kamhiyah 2195351
+		// // For Programming Concepts 1 EQ 2 – Fall 2021
+
+
+
+		int [] student= new int [100];
+		boolean [] status= new boolean [100]; 
+
+
+		for (int i = 0; i < student.length; i++) {
+			for (int j =i; j < status.length; j+= i+1) {
+				status [j]= !status [j];								
+				// now all lockers are open
+
+			}
+
+		}	
+
+		for (int j = 0; j < status.length; j++) {
+			if(status[j]) {													
+				// starts at index 1 (student 2). Make locker = student (locker 2).
+				// increment j (locker) by i + 1, because we are not incr index, but locker (index 0 is locker 1)
+				System.out.println("locker "+ (j+1) + " is open" );
+
+			} else 
+				System.out.print("");
+
+		}
+
+
+
+
+
+	}
+
+}
